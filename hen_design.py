0b56d16415a3560946074c5086e64de3ff44a87b
PedroSeber <pedroseber@hotmail.com>
2020-11-27 01:36:17 -0600

    Changed Streams to be a class; added the get_parameters() method, changed plotting to make_tid() method

--- hen_design.py
+++ hen_design.py
@@ -15,6 +15,7 @@ class HEN:
         self.flow_unit = flow_unit
         self.temp_unit = temp_unit
         self.cp_unit = cp_unit
+        self.first_utility = None
         self.streams = OrderedDict()
     
     def add_stream(self, t1, t2, cp, flow_rate = 1, stream_name = None, flow_unit = None, temp_unit = None, cp_unit = None):
@@ -34,26 +35,73 @@ class HEN:
                 idx += 1
             stream_name = f'{letter}{idx}'
 
-        stream_init = namedtuple('Stream', ['t1', 't2', 'cp', 'flow_rate'])
-        self.streams[stream_name] = stream_init(t1*temp_unit, t2*temp_unit, cp*cp_unit, flow_rate*flow_unit)
+        self.streams[stream_name] = Stream(t1, t2, cp, flow_rate, flow_unit, temp_unit, cp_unit)
 
-    def make_graph(self):
+    def get_parameters(self):
         """
-        This function receives two matrices representing the temperatures and C_P (heat capacity) values for a system of streams and
-        returns a Temperature Interval Diagram, a Composite Curve for both hot and cold streams, and the minimum utilities needed
+        This function obtains parameters (enthalpies, pinch temperature, heats above / below pinch) for the streams associated with this HEN object.
+        """
+        # Making unyt work since it doesn't allow multiplication of °C and °F
+        if self.temp_unit == unyt.degC:
+            local_temp_unit = unyt.delta_degC
+        elif self.temp_unit == unyt.degF:
+            local_temp_unit = unyt.delta_degF
+
+        # Starting array from class data
+        temperatures = np.empty( (len(self.streams), 2) )
+        cp_vals = np.empty( (len(self.streams), 1) )
+
+        for idx, values in enumerate(self.streams.items()): # values[0] has the stream names, values[1] has the properties
+            temperatures[idx, 0], temperatures[idx, 1] = values[1].t1.value, values[1].t2.value
+            cp_vals[idx, 0] = values[1].cp.value
         
-        Parameters
-        --------------------
-        temperatures: Nx2 array-like
-            Stream temperatures, where each row represents a stream, the first column represents the initial temperature, and the second
-            column represents the final temperature.
-            For hot streams, the first value is higher. For cold streams, the second value is higher
-        cp_vals: Nx1 array-like
-            The heat capacity of each stream, in the order the streams were passed
-        delta_t: double, optional, default = 10
-            The minimum temperature difference between the hot and cold streams
-        temp_unit: string, optional, default = " °C"
-            A string that represents the units of your temperature scale, used for graph labels
+        hot_streams = temperatures[:, 0] > temperatures[:, 1]
+        plotted_ylines = np.concatenate((temperatures[hot_streams, :].flatten(), temperatures[~hot_streams, :].flatten() + self.delta_t))
+        self._plotted_ylines = np.sort(np.unique(plotted_ylines))
+
+        # Getting the heat and enthalpies at each interval
+        tmp1 = np.atleast_2d(np.max(temperatures[hot_streams, :], axis = 1)).T >= np.atleast_2d(self._plotted_ylines[1:])
+        tmp2 = np.atleast_2d(np.min(temperatures[hot_streams, :], axis = 1)).T <= np.atleast_2d(self._plotted_ylines[:-1])
+        streams_in_interval1 = (tmp1 & tmp2).astype(np.int8) # Numpy treats this as boolean if I don't convert the type
+        tmp1 = np.atleast_2d(np.max(temperatures[~hot_streams, :], axis = 1)).T >= np.atleast_2d(self._plotted_ylines[1:] - self.delta_t)
+        tmp2 = np.atleast_2d(np.min(temperatures[~hot_streams, :], axis = 1)).T <= np.atleast_2d(self._plotted_ylines[:-1] - self.delta_t)
+        streams_in_interval2 = (tmp1 & tmp2).astype(np.int8)
+        streams_in_interval = np.concatenate((streams_in_interval1, -streams_in_interval2)) # Hot streams, then -cold streams
+        delta_plotted_ylines = self._plotted_ylines[1:] - self._plotted_ylines[:-1]
+        q_interval = np.sum(streams_in_interval * cp_vals * delta_plotted_ylines, axis = 0) # sum(FCp_hot - FCp_cold) * delta_t_interval
+        enthalpy_hot = np.sum(streams_in_interval1 * cp_vals[hot_streams] * delta_plotted_ylines, axis = 0) # sum(FCp_hot) * delta_t
+        enthalpy_cold = np.sum(streams_in_interval2 * cp_vals[~hot_streams] * delta_plotted_ylines, axis = 0) # sum(FCp_cold) * delta_t
+        
+        q_interval = q_interval[::-1] # Flipping the heat array so it starts from the top
+        q_sum = np.cumsum(q_interval)
+
+        if np.min(q_sum) <= 0:
+            first_utility = np.min(q_sum) # First utility is added to the minimum sum of heats, even if it isn't the first negative val
+            self.first_utility_loc = np.where(q_sum == first_utility)[0][0] # np.where returns a tuple that contains an array containing the location
+            self.first_utility = -first_utility * self.flow_unit*local_temp_unit*self.cp_unit # It's a heat going in, so we want it to be positive
+            q_sum[self.first_utility_loc:] = q_sum[self.first_utility_loc:] + self.first_utility.value
+            print('The first utility is %g %s, located after the interval %d\n' % (self.first_utility, self.first_utility.units, self.first_utility_loc+1))
+        else: # No pinch point
+            self.first_utility_loc = len(q_sum)
+            print('Warning: there is no pinch point nor a first utility\n')
+        
+        self.last_utility = -q_sum[-1] * self.flow_unit*local_temp_unit*self.cp_unit
+        self.enthalpy_hot = [0, enthalpy_hot] # The first value in enthalpy_hot is defined as 0
+        # Shifting the cold enthalpy so that the first value starts at positive last_utility
+        self.enthalpy_cold = np.insert(enthalpy_cold[:-1], 0, -self.last_utility)
+        print('The last utility is %g %s\n' % (self.last_utility, self.last_utility.units))
+
+        # Getting heats above / below pinch for each stream
+        q_above = np.sum(streams_in_interval[:, self.first_utility_loc:] * cp_vals * delta_plotted_ylines[self.first_utility_loc:], axis = 1) # Double check the location of the pinch cutoff [the first_utility_loc: thing]
+        q_below = np.sum(streams_in_interval[:, :self.first_utility_loc] * cp_vals * delta_plotted_ylines[:self.first_utility_loc], axis = 1)
+        for idx, elem in enumerate(self.streams):
+            self.streams[elem].q_above = q_above[idx] * self.first_utility.units
+            self.streams[elem].q_below = q_below[idx] * self.first_utility.units
+
+    def make_tid(self): # Add a show_middle_temps, show_q parameter for customization
+        """
+        This function plots a temperature-interval diagram using the streams and exchangers currently associated with this HEN object.
+        self.get_parameters() must be called before this function.
         """
         # Changing standard plotting options
         plt.rcParams['axes.titlesize'] = 5
@@ -79,103 +127,46 @@ class HEN:
         ax1.set_ylim(np.min(temperatures) - axis_delta, np.max(temperatures) + axis_delta)
         ax1.set_ylabel(f"Hot temperatures ({self.temp_unit})")
         ax1.set_yticks(np.linspace(np.min(temperatures) - axis_delta, np.max(temperatures) + axis_delta, 11))
+        q_above_text_loc = ax1.get_ylim()[1] - 0.01*(ax1.get_ylim()[1] - ax1.get_ylim()[0])
         
         
         # Manipulating the temperatures so that the hot and cold values are on the same y-position, even though they're shifted by delta_t
         plotted_ylines = np.empty(temperatures.size, dtype = 'object')
-        hot_streams = np.empty(len(temperatures), dtype = bool)
         for idx in range(len(temperatures)):
             if temperatures[idx, 0] > temperatures[idx, 1]:
                 my_color = 'r'
-                my_label1 = (str(temperatures[idx, 0]) + str(self.temp_unit) + " Hot side, " + str(temperatures[idx, 0]-self.delta_t) + str(self.temp_unit)
-                            + " Cold side")
-                my_label2 = (str(temperatures[idx, 1]) + str(self.temp_unit) + " Hot side, " + str(temperatures[idx, 1]-self.delta_t) + str(self.temp_unit)
-                            + " Cold side")
                 tplot1 = temperatures[idx, 0]
                 tplot2 = temperatures[idx, 1]
-                hot_streams[idx] = True
             else:
                 my_color = 'b'
-                my_label1 = (str(temperatures[idx, 0]+self.delta_t) + str(self.temp_unit) + " Hot side, " + str(temperatures[idx, 0]) + str(self.temp_unit)
-                            + " Cold side")
-                my_label2 = (str(temperatures[idx, 1]+self.delta_t) + str(self.temp_unit) + " Hot side, " + str(temperatures[idx, 1]) + str(self.temp_unit)
-                            + " Cold side")
                 tplot1 = temperatures[idx, 0] + self.delta_t
                 tplot2 = temperatures[idx, 1] + self.delta_t
 
             ax1.vlines(idx, tplot1, tplot2, color = my_color, linewidth = 0.25) # Vertical line for each stream
-            # Horizontal lines for each temperature
-            if tplot1 not in plotted_ylines: # Ensures we don't plot two of the same line, screwing-up the labels
-                ax1.axhline(tplot1, color = 'k', linewidth = 0.25)
-                ax1.text(np.mean(ax1.get_xlim()), tplot1, my_label1, ha = 'center', va = 'bottom')
-                plotted_ylines[idx] = tplot1
-            if tplot2 not in plotted_ylines:
-                ax1.axhline(tplot2, color = 'k', linewidth = 0.25)
-                #ax1.hlines(tplot2, ax1.get_xlim()[0], ax1.get_xlim()[1], color = 'k', linewidth = 0.5)
-                ax1.text(np.mean(ax1.get_xlim()), tplot2, my_label2, ha = 'center', va = 'bottom')
-                plotted_ylines[-1-idx] = tplot2; # Was -1-idx+1
+            q_above_text = r'$Q_{Top}$ = %g %s' % (self.streams[x_tick_labels[idx]].q_above, self.streams[x_tick_labels[idx]].q_above.units)
+            ax1.text(idx, q_above_text_loc, q_above_text, ha = 'center', va = 'top') # Heat above the pinch point
+            print(ax1.get_ylim())
+        
+        # Horizontal lines for each temperature
+        for elem in self._plotted_ylines:
+            ax1.axhline(elem, color = 'k', linewidth = 0.25)
+            my_label = str(elem) + str(self.temp_unit) + ' Hot side, ' + str(elem - self.delta_t) + str(self.temp_unit) + ' Cold side'
+            ax1.text(np.mean(ax1.get_xlim()), elem, my_label, ha = 'center', va = 'bottom')
+        
+        # Labeling the x-axis with the stream names
         ax1.set_xticks(range(len(temperatures)))
         ax1.set_xticklabels(x_tick_labels)
+
+        # Adding the pinch point
+        ax1.axhline(self._plotted_ylines[-2-self.first_utility_loc], color = 'k', linewidth = 0.5) # Arrays start at 0 but end at -1, so we need an extra -1 in this line and the next
+        ax1.text(np.mean(ax1.get_xlim()), self._plotted_ylines[-2-self.first_utility_loc] - 1, 'Pinch Point', ha = 'center', va = 'top')
         plt.show(block = False)
-        plotted_ylines = np.sort(plotted_ylines[plotted_ylines != None])
 
-        # Getting the heat and enthalpies at each interval
-        tmp1 = np.atleast_2d(np.max(temperatures[:sum(hot_streams), :], axis = 1)).T >= np.atleast_2d(plotted_ylines[1:])
-        tmp2 = np.atleast_2d(np.min(temperatures[:sum(hot_streams), :], axis = 1)).T <= np.atleast_2d(plotted_ylines[:-1])
-        streams_in_interval1 = (tmp1 & tmp2).astype(np.int8) # Numpy treats this as boolean if I don't convert the type
-        tmp1 = np.atleast_2d(np.max(temperatures[sum(hot_streams):, :], axis = 1)).T >= np.atleast_2d(plotted_ylines[1:] - self.delta_t)
-        tmp2 = np.atleast_2d(np.min(temperatures[sum(hot_streams):, :], axis = 1)).T <= np.atleast_2d(plotted_ylines[:-1] - self.delta_t)
-        streams_in_interval2 = (tmp1 & tmp2).astype(np.int8)
-        streams_in_interval = np.concatenate((streams_in_interval1, -streams_in_interval2)) # Hot streams, then -cold streams
-        delta_plotted_ylines = plotted_ylines[1:] - plotted_ylines[:-1]
-        q_interval = np.sum(streams_in_interval * cp_vals * delta_plotted_ylines, axis = 0) # sum(FCp_hot - FCp_cold) * delta_t_interval
-        enthalpy_hot = np.sum(streams_in_interval1 * cp_vals[:sum(hot_streams)] * delta_plotted_ylines, axis = 0) # sum(FCp_hot) * delta_t
-        enthalpy_cold = np.sum(streams_in_interval2 * cp_vals[sum(hot_streams):] * delta_plotted_ylines, axis = 0) # sum(FCp_cold) * delta_t
-        
-        q_interval = q_interval[::-1] # Flipping the heat array so it starts from the top
-        q_sum = np.cumsum(q_interval)
-
-        if np.min(q_sum) <= 0:
-            first_utility = np.min(q_sum) # First utility is added to the minimum sum of heats, even if it isn't the first negative val
-            first_utility_loc = np.where(q_sum == first_utility)[0][0] # np.where returns a tuple that contains an array containing the location
-            first_utility = -first_utility # It's a heat going in, so we want it to be positive
-            q_sum[first_utility_loc:] = q_sum[first_utility_loc:] + first_utility
-            ax1.axhline(plotted_ylines[-2-first_utility_loc], color = 'k', linewidth = 0.5) # Arrays start at 0 but end at -1, so we need an extra -1 in this line and the next
-            ax1.text(np.mean(ax1.get_xlim()), plotted_ylines[-2-first_utility_loc], 'Pinch Point', ha = 'center', va = 'top')
-            print('The first utility is %g, located after the interval %d\n' % (first_utility, first_utility_loc+1))
-            pass
-        else: # No pinch point
-            first_utility_loc = len(q_sum)
-            print('Warning: there is no pinch point nor a first utility\n')
-        
-        last_utility = -q_sum[-1]
-        enthalpy_hot = [0, enthalpy_hot] # The first value in enthalpy_hot is defined as 0
-        # Shifting the cold enthalpy so that the first value starts at positive last_utility
-        pdb.set_trace()
-        enthalpy_cold = np.insert(enthalpy_cold[:-1], 0, -last_utility)
-        print('The last utility is %g\n' % last_utility)
         """
         ax = gca;
         ti = ax.TightInset;
         ax.Position = [ti(1), ti(2), 1 - ti(1) - ti(3), 1 - ti(2) - ti(4)]; % Removing whitespace from the graph
         
-        
-
-        if min(q_sum) <= 0
-            [first_utility, first_utility_loc] = min(q_sum); % First utility is added to the minimum sum of heats, even if it isn't the first negative val
-            first_utility = -first_utility; % It's a heat going in, so we want it to be positive
-            q_sum(first_utility_loc:end) = q_sum(first_utility_loc:end) + first_utility;
-            yline(plotted_ylines(end-first_utility_loc), 'k', 'Pinch Point', 'FontWeight', 'bold', 'LabelHorizontalAlignment', 'center', 'LabelVerticalAlignment', 'bottom', 'LineWidth', 1.0);
-            fprintf('The first utility is %g, located after the interval %d\n', first_utility, first_utility_loc)
-        else % No pinch point
-            first_utility_loc = length(q_sum);
-            fprintf('Warning: there is no pinch point nor a first utility\n')
-        end
-        last_utility = -q_sum(end);
-        enthalpy_hot = [0, enthalpy_hot]; % The first value in enthalpy_hot is defined as 0
-        % Shifting the cold enthalpy so that the first value starts at positive last_utility
-        enthalpy_cold = [-last_utility, enthalpy_cold(1:end-1)];
-        fprintf('The last utility is %g\n', last_utility)
         
         % Adding heats and Cp values to the graph
         q_above = sum(streams_in_interval(:, end-first_utility_loc+1:end) .* cp_vals .* delta_plotted_ylines(end-first_utility_loc+1:end), 2);
@@ -196,9 +187,13 @@ class HEN:
         % There will still be issues if all streams on one side "skip" at least one interval
         plot(cumsum(enthalpy_hot), plotted_ylines(end-length(enthalpy_hot)+1:end), '-or')
         plot(cumsum(enthalpy_cold), plotted_ylines(1:length(enthalpy_cold)) - delta_t, '-ob')
-        
-        % TODO: adding heat exchangers automatically
-        fig3 = figure('Position', [10 10 1296 1080], 'DefaultAxesFontSize', 20);
-        copyobj(ax, fig3) % Copying figure 1
-        close % Temporary, since we don't have code here yet
         """
+
+class Stream():
+    def __init__(self, t1, t2, cp, flow_rate, flow_unit, temp_unit, cp_unit):
+        self.t1 = t1 * temp_unit
+        self.t2 = t2 * temp_unit
+        self.cp = cp * cp_unit
+        self.flow_rate = flow_rate * flow_unit
+        self.q_above = None
+        self.q_below = None
