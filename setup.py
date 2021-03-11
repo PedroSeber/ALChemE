c6faffdf2d167cd935d09ac2a238f6c71fc7e209
PedroSeber <32420230+PedroSeber@users.noreply.github.com>
2021-03-11 13:39:02 -0600

    Added setup.py and removed requirements.txt

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ALChemE',
+    author='Pedro Seber, Jonathan Hung, and Arjun Bansal',
+    author_email='pedro.seber@vanderbilt.edu',
+    license='MIT',
+    version='0.1.0',
+    description='A software suite to assist in Chemical Engineering design',
+    zip_safe=False,
+    packages = find_packages(),
+    install_requires = [
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'unyt',
+        'gekko',
+        'tkinter'
+    ],
+    )
