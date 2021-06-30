59015ac3ef1bfbb0e2c5d5e4ad1d858127cb42b7
PedroSeber <32420230+PedroSeber@users.noreply.github.com>
2021-06-29 22:51:43 -0300

    Bumped version to 0.4.1; added scipy requirement

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ALChemE',
-    author='Pedro Seber, Jonathan Hung, and Arjun Bansal',
-    author_email='pedro.seber@vanderbilt.edu',
-    license='MIT',
-    version='0.2.0',
-    description='A software suite to assist in Chemical Engineering design',
-    zip_safe=False,
+    name = 'ALChemE',
+    author = 'Pedro Seber, Jonathan Hung, and Arjun Bansal',
+    author_email = 'pedro.seber@vanderbilt.edu',
+    license = 'MIT',
+    version = '0.4.1',
+    description = 'A software suite to assist in Chemical Engineering design',
+    zip_safe = False,
     packages = find_packages(),
     install_requires = [
         'numpy',
@@ -15,6 +15,7 @@ setup(
         'matplotlib',
         'unyt',
         'gekko',
-        'joblib'
+        'joblib',
+        'scipy'
     ],
     )
