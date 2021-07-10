db95789253525bbd344ba8d4a4242acf140e5cb5
Jonathan Hung <jhung2021@gmail.com>
2021-07-09 20:29:09 -0500

    Add class descriptions and constraint explorer Treeview widget

--- hen_frontend_new.py
+++ hen_frontend_new.py
@@ -12,7 +12,7 @@ import subprocess
 import pdb
 
 ##############################################################################
-# WINDOW CLASS
+# PARENT WINDOW CLASS
 ##############################################################################
 class HEN_GUI(tk.Toplevel):
     '''
@@ -23,7 +23,7 @@ class HEN_GUI(tk.Toplevel):
         tk.Toplevel.__init__(self, parent)
         self.title('ALChemE - Heat exchanger network analysis tool')
     
-        # Initialize and pack HEN_gui_frame
+        # Initialize and pack HEN_GUI_frame
         self.HEN_GUI_frame = HEN_GUI_frame(parent=self)
         self.HEN_GUI_frame.pack()
 
@@ -32,7 +32,8 @@ class HEN_GUI(tk.Toplevel):
 ##############################################################################
 class HEN_GUI_frame(tk.Frame):
     '''
-    Parent frame of HEN program GUI. Child of HEN_GUI.
+    Parent frame of HEN program GUI. Child of HEN_GUI. Parent of all classes
+    in section: CHILD FRAME CLASSES.
     '''
     def __init__(self, parent=None):
         # Initialize and adjust frame settings
@@ -49,32 +50,51 @@ class HEN_GUI_frame(tk.Frame):
 ##############################################################################
 class HEN_GUI_input(tk.Frame):
     '''
-    Frame which holds user input. Child of HEN_GUI_frame.
+    Frame which contains the user input. Child of HEN_GUI_frame.
     '''
-    def __init__(self, parent=HEN_GUI_frame):
+    def __init__(self, HEN_GUI_frame, HEN_object):
         # Initialize and adjust Frame settings
-        tk.Frame.__init__(self, parent)
+        tk.Frame.__init__(self, parent=HEN_GUI_frame)
         
         # Define variables
+        self.HEN_object = HEN_object
         pass
 
 class HEN_GUI_object_explorer(tk.Treeview):
+    '''
+    Frame which contains the object explorer. Child of HEN_GUI_frame. Parent
+    of HEN_GUI_object_explorer_treeview.
+    '''
     def __init__(self):
         pass
 
 class HEN_GUI_terminal_display(tk.Text):
+    '''
+    Frame which contains the terminal display. Child of HEN_GUI_frame. Parent
+    of HEN_GUI_terminal_display_text.
+    '''
     def __init__(self):
         pass
 
 class HEN_GUI_graphics(tk.Frame):
+    '''
+    Frame which conatins the graphics tools. Child of HEN_GUI_frame.
+    '''
     def __init__(self):
         pass
 
 class HEN_GUI_optimizaion(tk.Frame):
+    '''
+    Frame which contains the optimization tools. Child of HEN_GUI_frame.
+    '''
     def __init__(self):
         pass
 
 class HEN_GUI_constraint_explorer(tk.Frame):
+    '''
+    Frame which contains the constraint explorer. Child of HEN_GUI_frame.
+    Parent of HEN_GUI_constraint_explorer_treeview.
+    '''
     def __init__(self):
         pass
 
@@ -82,10 +102,24 @@ class HEN_GUI_constraint_explorer(tk.Frame):
 # CHILD WIDGET CLASSES
 ##############################################################################
 class HEN_GUI_object_explorer_treeview(tk.Treeview):
+    '''
+    Treeview widget for the object explorer. Child of HEN_GUI_object_explorer.
+    '''
     def __init__(self):
         pass
 
 class HEN_GUI_terminal_display_text(tk.Text):
+    '''
+    Text widget for the terminal display. Child of HEN_GUI_terminal display.
+    '''
+    def __init__(self):
+        pass
+
+class HEN_GUI_constraint_explorer_treeview(tk.Treeview):
+    '''
+    Treeview widget for the constraint explorer. Child of 
+    HEN_GUI_object_explorer.
+    '''
     def __init__(self):
         pass
 
